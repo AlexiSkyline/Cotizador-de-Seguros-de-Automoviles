e1dd4fd7134568772a56da29493611d39f628f04
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-20 11:05:22 -0500

    we add styled components in the header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import styled from "@emotion/styled";
 import { Header } from "./components/Header";
+
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
 
 function App() {
   return (
-    <Header titulo='Cotizador de Seguros'/>
+    <Contenedor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+    </Contenedor>
   );
 }
 
